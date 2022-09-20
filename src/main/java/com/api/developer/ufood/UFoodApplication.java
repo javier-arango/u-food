a54267541bdd3f5f1d05820c3b649c3a611b8e52
Javier Arango <javierarango.developer@gmail.com>
2022-09-20 11:19:52 -0400

    Refactoring the project to add the backend

---
+++ src/main/java/com/api/developer/ufood/UFoodApplication.java
@@ -0,0 +1,13 @@
+package com.api.developer.ufood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UFoodApplication.class, args);
+	}
+
+}
