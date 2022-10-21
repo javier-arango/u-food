345e974fc07a70fe5f929ad29c116b44433d1d05
Javier Arango <javierarango.developer@gmail.com>
2022-10-21 19:23:53 -0400

    Change package name

--- src/main/java/com/api/ufood/UFoodApplication.java
+++ src/main/java/com/api/ufood/UFoodApplication.java
@@ -1,4 +1,4 @@
-package com.api.developer.ufood;
+package com.api.ufood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/api/ufood/controller/RestaurantController.java
+++ src/main/java/com/api/ufood/controller/RestaurantController.java
@@ -1,4 +1,4 @@
-package com.api.developer.ufood.controller;
+package com.api.ufood.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +12,7 @@ import java.util.Objects;
 @RestController
 @RequestMapping("/api/v1/")
 public class RestaurantController {
+
     private final String API_HOST = "https://api.yelp.com";
 
 
