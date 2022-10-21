855447eb0571b7029941b6c07f226bfdcce6fe5c
Javier Arango <javierarango.developer@gmail.com>
2022-10-21 19:04:56 -0400

    Added RestaurantModel and RestaurantController example

---
+++ src/test/java/com/api/developer/ufood/model/RestaurantModelTest.java
@@ -0,0 +1,10 @@
+package com.api.developer.ufood.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RestaurantModelTest {
+
+}
