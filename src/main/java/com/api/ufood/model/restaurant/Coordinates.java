d1a73180c9c7db37acb45fd6c4d7df79bd03586d
Javier Arango <javierarango.developer@gmail.com>
2022-10-21 22:00:03 -0400

    Refactoring the RestaurantModel

---
+++ src/main/java/com/api/ufood/model/restaurant/Coordinates.java
@@ -0,0 +1,12 @@
+package com.api.ufood.model.restaurant;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+public class Coordinates {
+    private double latitude;
+    private double longitude;
+}
