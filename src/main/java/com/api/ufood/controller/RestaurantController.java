c4f75a86c65e7410e6e79777f70016200edfba22
Javier Arango <javierarango.developer@gmail.com>
2022-10-22 01:56:13 -0400

    Refactoring the code

--- src/main/java/com/api/ufood/controller/RestaurantController.java
+++ src/main/java/com/api/ufood/controller/RestaurantController.java
@@ -3,20 +3,12 @@ package com.api.ufood.controller;
 import com.api.ufood.model.restaurant.Restaurant;
 import com.api.ufood.service.RestaurantService;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import org.springframework.http.HttpHeaders;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -27,15 +19,16 @@ public class RestaurantController {
         return "Hello World";
     }
 
-    @GetMapping("/countries")
-    public Object getRestaurant() {
-        // Make HTTP request to the Yelp API
+    @GetMapping("/restaurant/{id}")
+    public Restaurant getRestaurant(@PathVariable String id) {
         RestTemplateBuilder restTemplate = new RestTemplateBuilder();
 
+        // Make HTTP request to the Yelp API
         RestaurantService restaurantService = new RestaurantService(restTemplate);
 
 
-        return restaurantService.getRestaurant();
+        // Return the response
+        return restaurantService.getRestaurant(id);
     }
 
 }
