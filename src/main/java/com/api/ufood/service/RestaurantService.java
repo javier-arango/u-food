6ccff9485bbc8f5ba12d8efde0d7c79d3a60ce5d
Javier Arango <javierarango.developer@gmail.com>
2022-10-22 01:56:13 -0400

    Refactoring the code

--- src/main/java/com/api/ufood/service/RestaurantService.java
+++ src/main/java/com/api/ufood/service/RestaurantService.java
@@ -1,48 +1,46 @@
 package com.api.ufood.service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.stereotype.Service;
 
 import com.api.ufood.model.restaurant.Restaurant;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
 
 @Service
 public class RestaurantService {
-    // Attributes
+    private final String HOST_URL;
+    private final String API_KEY;
+
     private final RestTemplate restTemplate;
-    private final String URL = "";
-    private final String API_KEY = "";
+    private final HttpEntity<String> entity;
 
     // Constructor
     public RestaurantService(RestTemplateBuilder restTemplateBuilder) {
+        // Get rest template to do the http request
         this.restTemplate = restTemplateBuilder.build();
 
-        // Message in case there is an error getting the response
-        List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
-        messageConverters.add(converter);
+        // Load Environmental variables from .env
+        Dotenv dotenv = Dotenv.load();
+        this.HOST_URL = dotenv.get("YELP_HOST_URL");;
+        this.API_KEY = dotenv.get("YELP_API_KEY");
 
-        this.restTemplate.setMessageConverters(messageConverters);
-    }
-
-    public Object getRestaurant() {
         // Set header Authorization to the Yelp API Key
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(API_KEY);
 
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-
-        return restTemplate.exchange(URL, HttpMethod.GET, entity, Object.class).getBody();
+        // Request body
+        this.entity = new HttpEntity<>("body", headers);
     }
+
+    /**
+     * This method will return a Restaurant Object according to their id
+     * {@link #getRestaurant(String)}
+     * @return Restaurant
+     */
+    public Restaurant getRestaurant(String id) { return restTemplate.exchange(HOST_URL + id, HttpMethod.GET, entity, Restaurant.class).getBody(); }
 }
