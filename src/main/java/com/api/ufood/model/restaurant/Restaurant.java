0f03258ef88f6897007cc4a9be3c812bef3ab2ea
Javier Arango <javierarango.developer@gmail.com>
2022-10-21 23:56:40 -0400

    Able to get data from the Yelp api

--- src/main/java/com/api/ufood/model/restaurant/Restaurant.java
+++ src/main/java/com/api/ufood/model/restaurant/Restaurant.java
@@ -3,19 +3,13 @@ package com.api.ufood.model.restaurant;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.GenerationType;
 import java.util.List;
 
 
 @Data
 @NoArgsConstructor
 public class Restaurant {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
-
     private String name;
     private String url;
     private String phone;
