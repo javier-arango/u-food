865b6686ae8b8af104d1f97557615b684a922076
Javier Arango <javierarango.developer@gmail.com>
2022-10-24 15:17:45 -0400

    Added new end points to the API and refactor the code.

--- src/main/java/com/api/ufood/service/RestaurantService.java
+++ src/main/java/com/api/ufood/service/RestaurantService.java
@@ -1,9 +1,12 @@
 package com.api.ufood.service;
 
-import io.github.cdimascio.dotenv.Dotenv;
+import com.api.ufood.model.restaurant.Businesses;
+import com.api.ufood.model.restaurant.Restaurant;
+
+import com.api.ufood.model.restaurant.Reviews;
 import org.springframework.stereotype.Service;
 
-import com.api.ufood.model.restaurant.Restaurant;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -37,10 +40,48 @@ public class RestaurantService {
         this.entity = new HttpEntity<>("body", headers);
     }
 
+
+    /**
+     * This method will return a List of Restaurant Object according to the default search
+     * {@link #getAllRestaurants(String, int, String, int)}
+     * @return Restaurant
+     *
+     * @param limit | Number of business results to return. By default, it will return 10. Maximum is 50.
+     * @param sortBy | The results be sorted by one of the these modes: best_match, rating, review_count or distance. The default is distance.
+     * @param radius | A suggested search radius in meters. The max value is 40000 meters (about 25 miles). The default is 25 miles
+     *
+     */
+    public Businesses getAllRestaurants(String location, int limit, String sortBy, int radius) {
+        return restTemplate.exchange(
+                HOST_URL +
+                "search?location=" + location +
+                "&term=restaurants" +
+                "&limit=" + limit +
+                "&sort_by=" + sortBy +
+                "&radius=" + radius,
+                HttpMethod.GET, entity, Businesses.class).getBody();
+    }
+
+
     /**
      * This method will return a Restaurant Object according to their id
      * {@link #getRestaurant(String)}
      * @return Restaurant
      */
-    public Restaurant getRestaurant(String id) { return restTemplate.exchange(HOST_URL + id, HttpMethod.GET, entity, Restaurant.class).getBody(); }
+    public Restaurant getRestaurant(String id) {
+        // Get restaurant details
+        Restaurant restaurantDetails = restTemplate.exchange(HOST_URL + id, HttpMethod.GET, entity, Restaurant.class).getBody();
+
+        // Get Restaurant reviews
+        if (restaurantDetails != null) restaurantDetails.setReview(getReviews(restaurantDetails.getId()));
+
+        return restaurantDetails;
+    }
+
+    /**
+     * This method will return a Reviews Object according to the restaurant id
+     * {@link #getReviews(String)}
+     * @return Restaurant
+     */
+    public Reviews getReviews(String id) { return restTemplate.exchange(HOST_URL + id + "/reviews", HttpMethod.GET, entity, Reviews.class).getBody(); }
 }
