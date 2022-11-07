3f6efadd680f51c50a927cd94385a2a17a730d9d
Jean <fonrosejeangregor@gmail.com>
2022-11-07 04:09:35 -0500

    Created the Authentication component

--- client/src/components/Authentication/Authentication.js
+++ client/src/components/Authentication/Authentication.js
@@ -15,39 +15,45 @@ function Authentication() {
   return (
     <>
       <div className="container">
-        <h1 id="login-slogan">
-          Discover <br />
-          your restaurant of choice <br />
-          with
-        </h1>
-        <img className="logo" src={logo} alt="UFood logo" />
-        <form id="auth-form">
-          {isSignup && (
+        <div id="banner"></div>
+        <div className="sub-container">
+          <h1 id="login-slogan">
+            Discover <br />
+            your restaurant of choice <br />
+            with
+          </h1>
+          <img className="logo" src={logo} alt="UFood logo" />
+          <form id="auth-form">
+            {isSignup && (
+              <>
+                <input type="text" name="fname" placeholder="First Name" />
+                <input type="text" name="lname" placeholder="Last Name" />
+              </>
+            )}
+            <input type="email" name="email" placeholder="Email" />
+            <input type="password" name="password" placeholder="Password" />
+            <input
+              type="submit"
+              name="submit"
+              value={isSignup ? "Sign Up" : "Login"}
+            />
+          </form>
+
+          {!isSignup && (
             <>
-              <input type="text" name="fname" placeholder="First Name" />
-              <input type="text" name="lname" placeholder="Last Name" />
+              <a id="fg-pass" href="#">
+                Forgot password?
+              </a>
             </>
           )}
-          <input type="email" name="email" placeholder="Email" />
-          <input type="password" name="password" placeholder="Password" />
-          <input
-            type="submit"
-            name="submit"
-            value={isSignup ? "Sign Up" : "Login"}
-          />
-        </form>
 
-        {!isSignup && (
-          <>
-            <a id="fg-pass" href="#">
-              Forgot password?
+          <p id="no-account">
+            {isSignup ? "Have an account already" : "Don't have an account"}?{" "}
+            <a href="#" onClick={switchMode}>
+              {isSignup ? "Login" : "Sign up"}
             </a>
-          </>
-        )}
-
-        <p id="no-account">
-          {isSignup ? "Have an account already" : "Don't have an account"}? <a href="#" onClick={switchMode}>{isSignup ? "Login" : "Sign up"}</a>
-        </p>
+          </p>
+        </div>
       </div>
     </>
   );
