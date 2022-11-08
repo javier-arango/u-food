622e05416596ca426c5dce35dda2ef0e290c05f7
Jean <fonrosejeangregor@gmail.com>
2022-11-08 02:56:49 -0500

    Imported the BrowserRouter and wrapped App component with it

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-  BrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
+// CSS files
 import "./index.css";
+
+// Components
 import App from "./components/App/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
