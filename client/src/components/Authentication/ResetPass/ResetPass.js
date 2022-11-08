2f7ef2c9df45ff9ef1431aebe4e7d50a1fd1d66a
Jean <fonrosejeangregor@gmail.com>
2022-11-08 03:11:20 -0500

    Implemented the functions to handle the input when resetting the password

--- client/src/components/Authentication/ResetPass/ResetPass.js
+++ client/src/components/Authentication/ResetPass/ResetPass.js
@@ -5,6 +5,18 @@ import { Link } from "react-router-dom";
 import logo from "../../../assets/logo.png";
 
 function ResetPass() {
+  const [email, setEmail] = useState("");
+
+  // Handle when the input of the email is changed
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  // Handle when the user click the reset password button
+  const handleSubmit = () => {
+    console.log(email);
+  };
+
   return (
     <>
       <div className="container">
@@ -12,15 +24,16 @@ function ResetPass() {
         <div className="sub-container">
           <img className="logo" src={logo} alt="UFood logo" />
 
-          <p class="veri-msg">Enter the email associated with your account</p>
+          <p className="veri-msg">Enter the email associated with your account</p>
           <input
             id="email-reset"
             type="email"
             name="email"
             placeholder="Email"
+            onChange={handleChange}
           />
-          <button id="reset-btn">Reset Password</button>
-
+          <button type="submit" id="reset-btn" onClick={handleSubmit}>Reset Password</button>
+          <br/>
           <Link to="/">Return back to login</Link>
         </div>
       </div>
