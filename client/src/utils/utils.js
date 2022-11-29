c8a1bf19f676b3a263939d9a54a1000e0fcaddd9
JTucker17 <87397175+JTucker17@users.noreply.github.com>
2022-11-29 12:51:07 -0500

    Added distance feature to util
    Distance function to find deistance between user and restaurants given coordinates.

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -17,3 +17,17 @@ export const verifyPassword = function (password1, password2) {
 
     return result;
 }
+
+//expecting userLoc and restLoc to be objects with lat (latitude) and long (longitude)
+export const distanceToRest = function(userLoc, restLoc) {
+
+    const radEarth = 3963; //earth radius in miles
+    const p = 0.017453292519943295; //pi/180  as const for easier calculations
+
+    let val1 = 0.5 - Math.cos((restLoc.lat - userLoc.lat) * p) / 2
+        + Math.cos(userLoc.lat * p) * Math.cos(restLoc.lat * p) * (1 - Math.cos((restLoc.long - userLoc.long) * p)) / 2;
+
+    let distance = 2 * radEarth * Math.asin(Math.sqrt(val1));
+
+    return distance.toFixed(2) //returns total distance between user and restaurant in miles to 2 decimals
+}
