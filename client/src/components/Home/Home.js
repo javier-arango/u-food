e9d0227d2910c30df5b8db16543528acc17f8b5a
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2022-12-01 12:56:25 -0500

    Added a no image template, and fixed displaying favorite Icon and showing no image

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,9 +1,53 @@
 import React from "react";
 
+// CSS File
+import "./Home.css";
+
+// Assets
+import logo from "../../assets/logo.png";
+import noImage from "../../assets/no_image.png";
+import favoriteIcon from "../../assets/favorite.svg";
+
 function Home() {
   return (
     <>
-      <div>I am at the home page</div>
+      <div className="container">
+        <nav id="mobile-nav">
+          <img id="logo" src={logo} />
+        </nav>
+
+        <button id="search-btn">Search for Restaurants</button>
+
+        <div id="content">
+          <section className="restaurant-section">
+            <h3 className="title">Restaurants Nearby</h3>
+
+            <div className="restaurants-ctn">
+              <div className="restaurants">
+                <div className="restaurant">
+                  <div
+                    className="img-ctn"
+                    style={{ backgroundImage: `url(${noImage})` }}
+                  >
+                    <div className="icon-ctn">
+                      <img src={favoriteIcon} className="favorite-icon" />
+                    </div>
+                  </div>
+                  <h4 className="rst-name">Restaurant Name</h4>
+                  <div className="info">
+                    <p className="address">5168 SW 34th Gainesville FL 32608</p>
+                    <div className="review-info">
+                      <div className="tag">Latin</div>
+                      <p className="stars">4.3 stars</p>
+                      <p className="reviews-ctn">324 reviews</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
     </>
   );
 }
